5a33ca341715cf06c1c1439e1777745141c689b1
Dominik Widlak <dominik.widlak@microsoft.wsei.edu.pl>
2020-06-17 20:22:05 +0200

    BUMgame_and_SI_part1

--- 1000000_in_1/Program.cs
+++ 1000000_in_1/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Xml;
 using ClassLibrary;
 
 namespace _1000000_in_1
@@ -11,16 +13,17 @@ namespace _1000000_in_1
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
             bool badValue = false;
             int page = 0;
             while (true)
             {
                 badValue = startMsg(badValue);
 
-
-
+                int parametr = rnd.Next(2, 11);
+                int parametr2 = (page + 10) / 10;
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.WriteLine($"[{page + 1}]");
+                Console.WriteLine($"[{page + 1}] BUM - gracze naprzemiennie liczą od {parametr2}, w momencie gdy jakaś liczba jest podzielna lub zawiera w sobie {parametr} piszą \"BUM\"");
                 Console.WriteLine($"[{page + 2}]");
                 Console.WriteLine($"[{page + 3}]");
                 Console.WriteLine($"[{page + 4}]");
@@ -69,6 +72,7 @@ namespace _1000000_in_1
                     switch (int.Parse(bufor)-page)
                     {
                         case 1:
+                            BUM(parametr, parametr2);
                             break;
 
                         default:
@@ -119,6 +123,113 @@ namespace _1000000_in_1
             Console.WriteLine("Aby dokonać wyboru, wpisz Id gry");
 
             return badValue;
+        }
+
+        static void BUM(int bumINT, int Start)
+        {
+            int pktSI = 0;
+            int pktPlayer = 0;
+
+            BumGame.Regulamin(Start,bumINT);
+
+            while (true)
+            {
+                for (int i = Start; true; i += 2)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    Console.WriteLine($"Aktualny wynik: Komputer {pktSI}:{pktPlayer} Gracz");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine();
+
+                    string bufor = BumGame.Si(bumINT, i, Start);
+
+                    Console.WriteLine(bufor);
+                    Thread.Sleep(500);
+
+                    try
+                    {
+                        if (BumGame.Game(bumINT, int.Parse(bufor)))
+                        {
+                            Console.WriteLine("ojjj, moja wina :/ punkty lecą do cb.");
+                            pktPlayer++;
+                            Console.ReadKey();
+                            break;
+                        }
+                        //else if (int.Parse(bufor)!=i)
+                        //{
+                        //    Console.WriteLine("ojjj, moja wina :/ punkty lecą do cb.");
+                        //    pktPlayer++;
+                        //    Console.ReadKey();
+                        //    break;
+                        //}
+
+                    }
+
+                    catch (FormatException)
+                    {
+                        if (bufor != "BUM")
+                        {
+                            Console.WriteLine("ojjj, moja wina :/ punkty lecą do cb.");
+                            pktPlayer++;
+                            Console.ReadKey();
+                            break;                            
+                        }
+                    }
+
+                    
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    Console.WriteLine($"Aktualny wynik: Komputer {pktSI}:{pktPlayer} Gracz");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine();
+
+                    bufor = Console.ReadLine();
+
+                    try
+                    {
+                        if (BumGame.Game(bumINT, int.Parse(bufor)))
+                        {
+                            Console.WriteLine("Ha! Mam cię, ta runda jest moja");
+                            pktSI++;
+                            Console.ReadKey();
+                            break;
+                        }
+                        //else if (int.Parse(bufor) != i+1)
+                        //{
+                        //    Console.WriteLine("Ha! Mam cię, ta runda jest moja");
+                        //    pktSI++;
+                        //    Console.ReadKey();
+                        //    break;
+                        //}
+                    }
+
+                    catch(FormatException)
+                    {
+                        if(bufor != "BUM")
+                        {
+                            Console.WriteLine("Ha! Mam cię, ta runda jest moja");
+                            pktSI++;
+                            Console.ReadKey();
+                            break;
+                        }
+                    }
+
+                    //int bufor = BumGame.Game(bumINT);
+                    //if (bufor == 1)
+                    //{
+                    //    Console.WriteLine("ojjj, moja wina :/ punkty lecą do cb.");
+                    //    pktPlayer++;
+                    //    break;
+                    //}
+                    //else if (bufor == 2)
+
+                }
+
+                if (pktSI == 3) { Console.WriteLine("Wygrałem!"); break; }
+                if (pktPlayer == 3) { Console.WriteLine("Gratuluje wygranej!"); break; }
+            }
+
         }
     }
 }
---
+++ ClassLibrary/BumGame.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class BumGame
+    {
+        public static void Regulamin(int Start, int bumINT)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Witaj w grze BUM!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("Zasady są proste:");
+            Console.WriteLine($"1. Naprzemiennie liczymy od {Start}.");
+            Console.WriteLine($"2. W momencie kiedy napotkamy liczbę zawierającą lub podzielną przez {bumINT}, zamiast liczby piszemy \"BUM\".");
+            Console.WriteLine("3. Po wyświetleniu i zgaśnięciu liczby, wpisujesz następną.");
+            Console.WriteLine("4. Nie ma możliwości ponownego zobaczenia liczby, trzeba ją zapamiętać i podać kolejną.");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Po zapoznainu się z regulaminem naciśnij dowolny klawisz.");
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Skoro wyjaśniliśmy sobie zasady, pozwól że ja zacznę.");
+            Console.WriteLine("Gotowy?");
+            Console.ReadKey();
+        }
+
+        public static bool Game(int bumINT, int test)
+        {
+            if (test % bumINT == 0) return true;
+
+            char[] tests = test.ToString().ToCharArray();
+
+            for(int i = 0; i<tests.Length;i++)
+            {
+                if(tests[i] == Convert.ToChar(bumINT.ToString())) return true;
+            }
+
+            return false;
+        }
+
+        public static string Si(int bumINT ,int correctValue, int Start)
+        {
+            Random nerfSi = new Random();
+
+            int bufor = nerfSi.Next(1, 11);
+            if(bufor<7)
+            {
+                if (BumGame.Game(bumINT, correctValue)) return "BUM";
+                return correctValue.ToString();
+            }
+            else if(bufor==10)
+            {
+                return nerfSi.Next(Start, 1000000).ToString();
+            }
+
+            if (BumGame.Game(bumINT, correctValue)) return correctValue.ToString();
+            return "BUM";
+        }
+    }
+}
